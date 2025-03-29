8c7642769999c99d1d74fa3f3e444daca37098f6
Anitej Rao <aneetej@gmail.com>
2025-03-29 16:08:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Auth from "./pages/Auth";
-import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
 import { BrowserRouter, Routes, Route, useLocation, useNavigationType } from "react-router-dom";
 import { useEffect } from "react";
 import Index from "./pages/Index";
